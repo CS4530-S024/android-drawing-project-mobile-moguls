d4b7a615e3d4811fe399965db9f0f1d851df2428
Christian Anderson <CtoEtoA@gmail.com>
2024-03-19 20:13:47 -0600

    updated dependences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,25 +52,37 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-testing:2.7.0")
     implementation("androidx.room:room-common:2.6.1")
 
-    // testing
+    // Compose
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.3")
+    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    implementation("androidx.compose.ui:ui:1.6.3")
+    implementation("androidx.compose.ui:ui-graphics:1.6.3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3:1.2.1")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test:runner:1.5.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.3")
 
-
-    // viewmodel stuff
+    // Viewmodel
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 
-    // fragment stuff
+    // Fragment
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
-    // Color Picker Import
+    // Color Picker
     implementation("com.github.skydoves:colorpickerview:2.3.0")
 
-    // Room stuff
+    // Room Database
     implementation("androidx.room:room-common:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
